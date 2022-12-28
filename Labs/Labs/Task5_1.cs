0d8ebd474beaa014ffcba9ee283776b7c7d0f6e7
Dorofeya <feyasidorenkone@gmail.com>
2022-12-28 15:41:59 +0500

    task1_laba5
    commit task1

--- Labs/Labs/Task5_1.cs
+++ Labs/Labs/Task5_1.cs
@@ -8,5 +8,30 @@ namespace Labs
 {
     internal class Task5_1
     {
+        public static void Main()
+        {
+            string b = "5";
+            while (true)
+            {
+                var a = Console.ReadLine();
+                if (a == "q")
+                {
+                    break;
+                }
+                else if (int.TryParse(a, out var result))
+                {
+                    Console.WriteLine((char)result);
+                }
+                else if (float.TryParse(a, out var result2))
+                {
+
+                    if (Math.Abs(result2 - float.Parse(b)) < 0.0001)
+                    {
+                        break;
+                    }
+                }
+                b = a;
+            }
+        }
     }
 }
